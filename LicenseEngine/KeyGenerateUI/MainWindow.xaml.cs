375b1049c08e80ec27ada9c89d16cc23e9f3356f
Joween Flores <4754752+joweenflores@users.noreply.github.com>
2020-02-04 14:53:07 +0800

    Code Optimization

--- LicenseEngine/KeyGenerateUI/MainWindow.xaml.cs
+++ LicenseEngine/KeyGenerateUI/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Windows;
-using KeyGenerate;
+﻿using System.Windows;
 using KeyCommon;
+using KeyGenerate;
 using MahApps.Metro.Controls;
 
 namespace KeyGenerateUI
@@ -18,29 +17,25 @@ namespace KeyGenerateUI
 
         private void btnGenerate_Click(object sender, RoutedEventArgs e)
         {
-            if (int.TryParse(txtProdID.Text.ToString(), out var ID))
-            {
+            if (int.TryParse(txtProdID.Text, out var ID))
                 txtLicense.Text = GenerateKey(ID);
-            }
             else
-            {
                 txtLicense.Text = "ERROR: Product ID must be an integer.";
-            }
         }
 
         private string GenerateKey(int ID)
         {
             var keyGenerator = new KeyGenerator();
 
-            var keyBytes = new[] {
-
+            var keyBytes = new[]
+            {
                 new KeyByteSet(1, 254, 122, 96),
                 new KeyByteSet(2, 54, 124, 222),
                 new KeyByteSet(3, 119, 142, 132),
                 new KeyByteSet(4, 128, 122, 10),
                 new KeyByteSet(5, 165, 15, 132),
                 new KeyByteSet(6, 128, 175, 213),
-                new KeyByteSet(7, 7, 244, 132 ),
+                new KeyByteSet(7, 7, 244, 132),
                 new KeyByteSet(8, 128, 122, 251)
             };
 
